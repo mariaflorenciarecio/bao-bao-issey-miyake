8a6e4547d28b74294c3b1aae54e8e8d7d3416486
Florencia <mariaflorenciarecio@gmail.com>
2022-01-20 16:21:04 -0300

    items del carrito renderizados

--- js/cart/addToCartDOM.js
+++ js/cart/addToCartDOM.js
@@ -1,7 +1,31 @@
 import { formatPrice } from '../utils.js';
 
-const addToCartDOM = (product) => {
-    console.log(product);
+const cartItemsDOM = document.getElementById('cart-items');
+
+const addToCartDOM = ({id, name, price, collection, color, amount}) => {
+    const div = document.createElement('div');
+    div.classList.add('item');
+    div.setAttribute('data-id', id);
+    div.innerHTML = `
+        <div class="item__image">
+            <img src="./assets/img/tienda/${name}-${collection}-${color}.jpg" alt="${name} ${collection} Color ${color}">
+        </div>
+        <div class="item__data">
+            <div class="item__bin" data-id="${id}">
+                <i class="far fa-trash-alt"></i>
+            </div>
+            <h3><a href="#">${name} ${collection}</a></h3>
+            <h4>${color}</h4>
+            <div class="item__units">
+                <i class="fas fa-minus" data-id="${id}"></i>
+                <strong data-id="${id}">${amount}</strong>
+                <i class="fas fa-plus" data-id="${id}"></i>
+            </div>
+            <span class="item__subtotal">${formatPrice(price)} c/u</span>
+            <strong class="item__total">${formatPrice(price*amount)}</strong>
+        </div>
+    `;
+    cartItemsDOM.appendChild(div);
 };
 
 export default addToCartDOM;
